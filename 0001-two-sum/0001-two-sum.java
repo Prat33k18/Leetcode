895435a473775ec2e527678108f7d8d081dbe95a
Prat33k18 <136463055+Prat33k18@users.noreply.github.com>
2024-07-19 11:31:57 +0530

    Time: 46 ms (18.2%), Space: 44.9 MB (22.88%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 4, 7, 9};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
+   
